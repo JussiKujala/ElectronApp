a2f0638e0ee3593b1779927ba48c3bef4f489df0
JussiKujala <jussijustus@gmail.com>
2025-04-26 23:38:29 +0300

    {react integraatio}

--- main.js
+++ main.js
@@ -1,12 +1,36 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const isDev = require('electron-is-dev'); // Tämä paketti tunnistaa kehitysympäristön
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
     width: 800,
-    height: 600
-  })
+    height: 600,
+    webPreferences: {
+      nodeIntegration: false,   // Turvallisuusasetukset: älä ota Node-integrationia käyttöön selaimessa
+      contextIsolation: true      // Suojaa konteksteja toisiltaan
+    }
+  });
 
-  win.loadFile('index.html')
+  if (isDev) {
+    // Kehitysympäristössä ladat Reactin dev-palvelin
+    win.loadURL('http://localhost:3000');
+  } else {
+    // Tuotannossa ladataan valmiiksi rakennettu React sovellus
+    win.loadFile(path.join(__dirname, 'frontend', 'build', 'index.html'));
+  }
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
